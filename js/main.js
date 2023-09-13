7aa402c851ceb495f4397e8d353e21bed15314ba
viniciuss_Frota <viniciussantosfrota10012017@gmail.com>
2023-09-13 19:29:54 -0300

    responsivo
    criação do arquivo específico para responsivo, além do aperfeiçoamento das páginas de entrar e criação de conta, aplicando a responsividade em ambas as páginas

--- js/main.js
+++ js/main.js
@@ -89,21 +89,3 @@ document.addEventListener("scroll", handleScroll);
 function enviado(){
     window.alert('Sua dúvida foi enviada com sucesso!')
 }
-//animação das seções 
-const boxes = document.querrySelectorAll(".box");
-window.addEventListener("scroll", checkBoxes);
-
-checkBoxes();
-
-function checkBoxes(){
-    const triggerBottom = window.innerHeight / 5 * 4;
-    boxes.forEach((box, idx) => {
-        const boxTop = box.getBoundingClientRect().top;
-
-        if(boxTop < triggerBottom){
-            box.classList.add("show");
-        }else{
-            box.classList.remove("show");
-        }
-    });
-}
